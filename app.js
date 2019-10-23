0ee71b92c5870833d687631cafb1a8c846340749
Dawid <dawid.budaszewski1@gmail.com>
2019-10-23 20:16:02 +0100

    saving before i mess up

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const characteristic = [["Jean-Claude",["Male","Glasses","Brown eyes","Bald","White hair","Small mouth","Small nose"]],
+["Pierre",["Male","Mustache","Brown eyes","Brown hair","Big mouth","Small nose"]],
+["Jean",["Male","White hair","Big nose","Big mouth","Blue eyes"]],
+["Amelie",["Female","Hat","Brown hair","Small mouth","Long hair","Brown eyes","Small nose"]],
+["Mirabelle",["Female","Black hair","Earrings","Small mouth","Brown eyes","Big nose"]],
+["Isabelle",["Female","Blonde hair","Glasses","Hat","Small mouth","Small nose","Brown eyes"]],
+["Antonin",["Male","Brown eyes","Black hair","Small nose","Big mouth"]],
+["Bernard",["Male","Brown eyes","Brown hair","Small nose","Hat"]],
+["Owen",["Male","Blue eyes","Blonde hair","Small nose","Small mouth"]],
+["Dylan",["Male","Brown eyes","Blonde hair","Small nose","Small mouth","Bald","Beard"]],
+["Herbert",["Male","Brown eyes","Blonde hair","Big nose","Small mouth","Bald"]],
+["Christine",["Female","Blue eyes","Blonde hair","Small nose","Small mouth","Long hair"]],
+["Luc",["Male","Brown eyes","White hair","Small nose","Small mouth","Glasses"]],
+["Cecilian",["Male","Brown eyes","Ginger hair","Small nose","Small mouth"]],
+["Lionel",["Male","Brown eyes","Brown hair","Big nose","Big mouth","Mustache"]],
+["Benoit",["Male","Brown eyes","Brown hair","Small mouth","Small nose","Mustache","Beard"]],
+["Robert",["Male","Blue eyes","Brown hair","Big nose","Big mouth"]],
+["Charline",["Female","Brown hair","White hair","Small nose","Big mouth"]],
+["Renaud",["Male","Brown eyes","Blonde hair","Small nose","Big mouth","Mustache"]],
+["Michel",["Male","Brown eyes","Blonde hair","Small nose","Big mouth","Beard"]],
+["Pierre-Louis",["Male","Blue eyes","Brown hair","Small nose","Small mouth","Bald","Glasses"]],
+["Etienne",["Male","Brown eyes","Blonde hair","Small nose","Small mouth","Glasses"]],
+["Henri",["Male","Brown eyes","White hair","Small nose","Big mouth","Hat"]],
+["Damien",["Male","Brown eyes","Blonde hair","Small nose","Big mouth","Hat"]]]
+
 const maleBtn = document.querySelector('.male')
 const femaleBtn = document.querySelector('.female')
 
@@ -24,11 +49,35 @@ const buttonsForValues = [maleBtn, femaleBtn, brownEyesBtn, blueEyesBtn, baldBtn
 
 let currentBtnValue 
 
+let values = []
+
+const object = {
+    male: 1, female: 2, brownEyes: 3, 
+}
+
+function checkWhichCharactersHaveTheSamePropertyAsCurrentBtnValue (){
+    characteristic.forEach(array=>{
+        array[1].forEach(property=>{
+
+            if (){
+                values.push(property)
+            } else {
+                console.log('lalala')
+            }
+        })
+    })
+}
+
+
 buttonsForValues.forEach(button =>{
     button.addEventListener('click', () =>{
-        currentBtnValue = button.value
+        currentBtnValue = button.values
+        checkWhichCharactersHaveTheSamePropertyAsCurrentBtnValue()
     })
+
 })
 
 
-// write a loop nested in a loop which is checking if an element belongs to the set of characteristics and add the logic to the game
+
+
+
